58428eb9cd48e0e5e67554bbb4f62e4b1a04c37d
Pranav Satish Kharote <pranavkharote2005@gmail.com>
2025-07-11 15:44:31 +0530

    backend.env check

--- backend/server.js
+++ backend/server.js
@@ -16,19 +16,24 @@ const allowedOrigins = [
   'https://smart-prep-azure.vercel.app', // ✅ Your deployed frontend
 ];
 
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error('Not allowed by CORS'));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: true, // 🔓 Allows all origins temporarily
     credentials: true,
   })
 );
-
 app.use(cookieParser());
 app.use(express.json());
 
