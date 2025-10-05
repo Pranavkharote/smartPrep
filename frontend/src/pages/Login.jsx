b8be058e137f8485e3c5e880d96d63e396e62ff9
Pranav Satish Kharote <pranavkharote2005@gmail.com>
2025-10-05 14:17:18 +0530

    Update Login and Signup buttons to display loading text instead of spinner for improved user experience

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -125,28 +125,7 @@ const Login = () => {
             />
           </div>
           <button className="w-full bg-green-500 hover:bg-green-600 text-white font-semibold py-2 rounded-md transition-colors duration-200">
-           {loading && (
-          <svg
-            className="animate-spin h-5 w-5 text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            ></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-            ></path>
-          </svg>
-        )}
+        
             {loading? "Logging In..." : "Login"}
           </button>
         </form>
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -132,28 +132,7 @@ function Signup() {
             // className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-green-500 focus:ring-1 focus:ring-green-500 opacity-80"
             className="w-full bg-green-600 text-white py-2 rounded hover:bg-green-700"
           >
-          {loading && (
-          <svg
-            className="animate-spin h-5 w-5 text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            ></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-            ></path>
-          </svg>
-        )}
+     
             {loading? "Signing Up..." : "Sign Up"}
           </button>
         </form>
